5a668df460d58234de16e8a73cda95a1d742e9e0
NebelungID <nebelung.id@outlook.com>
2026-07-01 18:52:32 +0300

    Форматирование: stylua (csv, enum, json) + обновление action_log

--- enum.lua
+++ enum.lua
@@ -1,71 +1,139 @@
 --[[
 
+
+
   Copyright 2017 Stefano Mazzucco
+
+
 
   Licensed under the Apache License, Version 2.0 (the "License");
 
+
+
   you may not use this file except in compliance with the License.
+
+
 
   You may obtain a copy of the License at
 
+
+
   http://www.apache.org/licenses/LICENSE-2.0
+
+
 
   Unless required by applicable law or agreed to in writing, software
 
+
+
   distributed under the License is distributed on an "AS IS" BASIS,
+
+
 
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
+
+
   See the License for the specific language governing permissions and
 
+
+
   limitations under the License.
+
+
 
 ]]
 --[[-
 
+
+
   @module enum
+
+
 
   @author Stefano Mazzucco
 
+
+
   @copyright 2017 Stefano Mazzucco
+
+
 
   @license Apache v2.0
 
+
+
   @usage
+
+
 
   enum = require("enum")
 
+
+
   sizes = {"SMALL", "MEDIUM", "BIG"}
+
+
 
   Size = enum.new("Size", sizes)
 
+
+
   print(Size) -- "<enum 'Size'>"
+
+
 
   print(Size.SMALL) -- "<Size.SMALL: 1>"
 
+
+
   print(Size.SMALL.name) -- "SMALL"
+
+
 
   print(Size.SMALL.value) -- 1
 
+
+
   assert(Size.SMALL ~= Size.BIG) -- true
+
+
 
   assert(Size.SMALL < Size.BIG) -- error "Unsupported operation"
 
+
+
   assert(Size[1] == Size.SMALL) -- true
+
+
 
   Size[5] -- error "Invalid enum member: 5"
 
+
+
   -- Enums cannot be modified
+
+
 
   Size.MINI -- error "Invalid enum: MINI"
 
+
+
   assert(Size.BIG.something == nil) -- true
+
+
 
   Size.MEDIUM.other = 1 -- error "Cannot set fields in enum value"
 
+
+
   -- Keys cannot be reused
 
+
+
   Color = enum.new("Color", {"RED", "RED"}) -- error "Attempted to reuse key: 'RED'"
+
+
 
 ]]
 local string = string
