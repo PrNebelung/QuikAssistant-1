588ccf313a0d235ecfa09afa159bb03d23d7d114
NebelungID <nebelung.id@outlook.com>
2026-07-02 14:36:13 +0300

    refactor: encapsulate global state in Engine table

--- StartEngine.lua
+++ StartEngine.lua
@@ -9,18 +9,92 @@ require("Assistant")
 
 log = require("log")
 
-isRun = true
+local Engine = {}
+
 transId = os.time()
 
-N_TransReplies = {}
-N_LastTransID = 0
-N_Orders = {}
-N_LastOrderNum = 0
-N_Trades = {}
-N_LastTradeNum = 0
+Engine.isRun = true
+
+Engine.N_TransReplies = {}
+Engine.N_LastTransID = 0
+Engine.N_Orders = {}
+Engine.N_LastOrderNum = 0
+Engine.N_Trades = {}
+Engine.N_LastTradeNum = 0
+
+Engine.OnInit = function()
+  if N_OnInit ~= nil then
+    N_OnInit()
+  end
+end
+
+Engine.OnStop = function()
+  Engine.isRun = false
+  if N_OnStop ~= nil then
+    N_OnStop()
+  end
+end
+
+Engine.OnClose = function()
+  if N_OnClose ~= nil then
+    N_OnClose()
+    sleep(1000)
+  end
+end
+
+Engine.OnTransReply = function(trans_reply)
+  for i, TransReply in ipairs(Engine.N_TransReplies) do
+    if Engine.N_TransReplies[i].trans_id == trans_reply.trans_id then
+      if Engine.N_TransReplies[i].checked ~= nil then
+        trans_reply.checked = true
+      end
+      table.sremove(Engine.N_TransReplies, i)
+      table.sinsert(Engine.N_TransReplies, trans_reply)
+      return
+    end
+  end
+  if Engine.N_LastTransID < trans_reply.trans_id then
+    table.sinsert(Engine.N_TransReplies, trans_reply)
+  end
+end
+
+Engine.OnOrder = function(order)
+  for i, Order in ipairs(Engine.N_Orders) do
+    if Engine.N_Orders[i].trans_id == order.trans_id then
+      if Engine.N_Orders[i].checked ~= nil then
+        order.checked = true
+      end
+      if Engine.N_Orders[i].last_execution_count ~= nil then
+        order.last_execution_count = Engine.N_Orders[i].last_execution_count
+      end
+      table.sremove(Engine.N_Orders, i)
+      table.sinsert(Engine.N_Orders, order)
+      return
+    end
+  end
+  if Engine.N_LastOrderNum < order.order_num then
+    table.sinsert(Engine.N_Orders, order)
+  end
+end
+
+Engine.OnTrade = function(trade)
+  for i, Trade in ipairs(Engine.N_Trades) do
+    if Engine.N_Trades[i].trade_num == trade.trade_num then
+      if Engine.N_Trades[i].checked ~= nil then
+        trade.checked = true
+      end
+      table.sremove(Engine.N_Trades, i)
+      table.sinsert(Engine.N_Trades, trade)
+      return
+    end
+  end
+  if Engine.N_LastTradeNum < trade.trade_num then
+    table.sinsert(Engine.N_Trades, trade)
+  end
+end
 
 function main()
-  while isRun do
+  while Engine.isRun do
     local loopOk, loopErr = pcall(function()
       if N_OnMainLoop ~= nil then
         N_OnMainLoop()
@@ -28,37 +102,37 @@ function main()
 
       ClearPositionCache()
 
-      for i, TransReplie in ipairs(N_TransReplies) do
-        if N_TransReplies[i].checked == nil then
-          if N_TransReplies[i].status > 1 and N_TransReplies[i].status ~= TRANS_STATUS_COMPLETED then
+      for i, TransReplie in ipairs(Engine.N_TransReplies) do
+        if Engine.N_TransReplies[i].checked == nil then
+          if Engine.N_TransReplies[i].status > 1 and Engine.N_TransReplies[i].status ~= TRANS_STATUS_COMPLETED then
             if N_OnTransExecutionError ~= nil then
-              N_OnTransExecutionError(N_TransReplies[i])
+              N_OnTransExecutionError(Engine.N_TransReplies[i])
             end
-            N_TransReplies[i].checked = true
-          elseif N_TransReplies[i].status == TRANS_STATUS_COMPLETED then
+            Engine.N_TransReplies[i].checked = true
+          elseif Engine.N_TransReplies[i].status == TRANS_STATUS_COMPLETED then
             if N_OnTransOK ~= nil then
-              N_OnTransOK(N_TransReplies[i])
+              N_OnTransOK(Engine.N_TransReplies[i])
             end
-            N_TransReplies[i].checked = true
+            Engine.N_TransReplies[i].checked = true
           end
         end
       end
 
-      for i, Order in ipairs(N_Orders) do
-        if N_Orders[i].checked == nil then
+      for i, Order in ipairs(Engine.N_Orders) do
+        if Engine.N_Orders[i].checked == nil then
           if N_OnNewOrder ~= nil then
-            N_OnNewOrder(N_Orders[i])
+            N_OnNewOrder(Engine.N_Orders[i])
           end
-          N_Orders[i].checked = true
+          Engine.N_Orders[i].checked = true
         end
-        local ExecutionCount = N_Orders[i].qty - N_Orders[i].balance
+        local ExecutionCount = Engine.N_Orders[i].qty - Engine.N_Orders[i].balance
         if
-          (N_Orders[i].last_execution_count == nil or N_Orders[i].last_execution_count ~= ExecutionCount)
+          (Engine.N_Orders[i].last_execution_count == nil or Engine.N_Orders[i].last_execution_count ~= ExecutionCount)
           and ExecutionCount > 0
         then
           if N_OnExecutionOrder ~= nil then
-            N_OnExecutionOrder(N_Orders[i])
-            N_Orders[i].last_execution_count = ExecutionCount
+            N_OnExecutionOrder(Engine.N_Orders[i])
+            Engine.N_Orders[i].last_execution_count = ExecutionCount
           end
         end
       end
@@ -67,35 +141,35 @@ function main()
       local ordersToRemove = {}
       local repliesToRemove = {}
 
-      for i, Trade in ipairs(N_Trades) do
-        N_OnNewTrade(N_Trades[i])
-        N_LastTradeNum = N_Trades[i].trade_num
-        tradesToRemove[N_Trades[i].trade_num] = true
+      for i, Trade in ipairs(Engine.N_Trades) do
+        N_OnNewTrade(Engine.N_Trades[i])
+        Engine.N_LastTradeNum = Engine.N_Trades[i].trade_num
+        tradesToRemove[Engine.N_Trades[i].trade_num] = true
 
         log.debug(
           "N_OnNewTrade() deal #"
-            .. N_Trades[i].trade_num
+            .. Engine.N_Trades[i].trade_num
             .. " trans_id="
-            .. tostring(N_Trades[i].trans_id)
+            .. tostring(Engine.N_Trades[i].trans_id)
             .. " price="
-            .. N_Trades[i].price
+            .. Engine.N_Trades[i].price
             .. " qty="
-            .. N_Trades[i].qty
+            .. Engine.N_Trades[i].qty
         )
-        log.trace(json.encode(N_Trades[i]))
+        log.trace(json.encode(Engine.N_Trades[i]))
 
-        if N_Trades[i].order_num ~= nil then
-          for j, Order in ipairs(N_Orders) do
-            if N_Trades[i].order_num == N_Orders[j].order_num then
-              if N_Orders[j].last_execution_count ~= nil and N_Orders[j].last_execution_count == N_Orders[j].qty then
-                N_LastTransID = N_Orders[j].trans_id
-                ordersToRemove[N_Orders[j].order_num] = true
-                for k, TransReply in ipairs(N_TransReplies) do
-                  if TransReply.trans_id == N_Orders[j].trans_id then
+        if Engine.N_Trades[i].order_num ~= nil then
+          for j, Order in ipairs(Engine.N_Orders) do
+            if Engine.N_Trades[i].order_num == Engine.N_Orders[j].order_num then
+              if Engine.N_Orders[j].last_execution_count ~= nil and Engine.N_Orders[j].last_execution_count == Engine.N_Orders[j].qty then
+                Engine.N_LastTransID = Engine.N_Orders[j].trans_id
+                ordersToRemove[Engine.N_Orders[j].order_num] = true
+                for k, TransReply in ipairs(Engine.N_TransReplies) do
+                  if TransReply.trans_id == Engine.N_Orders[j].trans_id then
                     repliesToRemove[k] = true
                   end
                 end
-                N_LastOrderNum = N_Orders[j].order_num
+                Engine.N_LastOrderNum = Engine.N_Orders[j].order_num
                 break
               end
             end
@@ -103,19 +177,19 @@ function main()
         end
       end
 
-      for i = #N_TransReplies, 1, -1 do
+      for i = #Engine.N_TransReplies, 1, -1 do
         if repliesToRemove[i] then
-          table.remove(N_TransReplies, i)
+          table.remove(Engine.N_TransReplies, i)
         end
       end
-      for i = #N_Orders, 1, -1 do
-        if ordersToRemove[N_Orders[i].order_num] then
-          table.remove(N_Orders, i)
+      for i = #Engine.N_Orders, 1, -1 do
+        if ordersToRemove[Engine.N_Orders[i].order_num] then
+          table.remove(Engine.N_Orders, i)
         end
       end
-      for i = #N_Trades, 1, -1 do
-        if tradesToRemove[N_Trades[i].trade_num] then
-          table.remove(N_Trades, i)
+      for i = #Engine.N_Trades, 1, -1 do
+        if tradesToRemove[Engine.N_Trades[i].trade_num] then
+          table.remove(Engine.N_Trades, i)
         end
       end
 
@@ -128,73 +202,11 @@ function main()
   end
 end
 
-function OnTransReply(trans_reply)
-  for i, TransReply in ipairs(N_TransReplies) do
-    if N_TransReplies[i].trans_id == trans_reply.trans_id then
-      if N_TransReplies[i].checked ~= nil then
-        trans_reply.checked = true
-      end
-      table.sremove(N_TransReplies, i)
-      table.sinsert(N_TransReplies, trans_reply)
-      return
-    end
-  end
-  if N_LastTransID < trans_reply.trans_id then
-    table.sinsert(N_TransReplies, trans_reply)
-  end
-end
+OnInit = Engine.OnInit
+OnStop = Engine.OnStop
+OnClose = Engine.OnClose
+OnTransReply = Engine.OnTransReply
+OnOrder = Engine.OnOrder
+OnTrade = Engine.OnTrade
 
-function OnOrder(order)
-  for i, Order in ipairs(N_Orders) do
-    if N_Orders[i].trans_id == order.trans_id then
-      if N_Orders[i].checked ~= nil then
-        order.checked = true
-      end
-      if N_Orders[i].last_execution_count ~= nil then
-        order.last_execution_count = N_Orders[i].last_execution_count
-      end
-      table.sremove(N_Orders, i)
-      table.sinsert(N_Orders, order)
-      return
-    end
-  end
-  if N_LastOrderNum < order.order_num then
-    table.sinsert(N_Orders, order)
-  end
-end
-
-function OnTrade(trade)
-  for i, Trade in ipairs(N_Trades) do
-    if N_Trades[i].trade_num == trade.trade_num then
-      if N_Trades[i].checked ~= nil then
-        trade.checked = true
-      end
-      table.sremove(N_Trades, i)
-      table.sinsert(N_Trades, trade)
-      return
-    end
-  end
-  if N_LastTradeNum < trade.trade_num then
-    table.sinsert(N_Trades, trade)
-  end
-end
-
-function OnInit()
-  if N_OnInit ~= nil then
-    N_OnInit()
-  end
-end
-
-function OnStop()
-  isRun = false
-  if N_OnStop ~= nil then
-    N_OnStop()
-  end
-end
-
-function OnClose()
-  if N_OnClose ~= nil then
-    N_OnClose()
-    sleep(1000)
-  end
-end
+return Engine
